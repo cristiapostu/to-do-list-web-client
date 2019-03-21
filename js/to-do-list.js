fa1eea45d1588482ba15864c749ae76e75a4f488
Cristian Apostu <cristy_apostu@yahoo.com>
2019-03-21 23:07:31 +0200

    fixed create item function, loading data from server, added delete function

--- js/to-do-list.js
+++ js/to-do-list.js
@@ -1,11 +1,10 @@
-
 window.ToDoList = {
 
-    apiURL: "http://localhost:8070/to-do-list",
+    apiURL: "http://localhost:8070/to-do-items",
 
     addItem: function () {
-        var description = $("input[title= 'Description']").val ();
-        var deadline = $("input[title= 'Deadline']").val ();
+        var description = $("input[title= 'Description']").val();
+        var deadline = $("input[title= 'Deadline']").val();
 
         var data = {
 
@@ -18,26 +17,79 @@ window.ToDoList = {
             method: "POST",
             contentType: "application/json; charset=utf-8",
             data: JSON.stringify(data)
-        }.done(function (response) {
+        }).done(function (response) {
             console.log(response);
-        //  reload items table
+            //  reload items table
 
-    }));
+        });
+    },
+
+    getItemRow: function (item) {
+        var deadline = new Date(item.deadline).toLocaleDateString('ro-RO');
+        var checkedAttribute = item.done ? 'checked' : '';
+        return `<tr>
+<td class="description">${item.description}</td>
+<td class="deadline">${deadline}</td>
+<td><input type="checkbox" ${checkedAttribute} class="mark-done" title="done"></td>
+<td><a href="#" class="fa fa-trash delete" data-id="${item.id}"></a></td>
+</tr>`
+    },
+    displayItems: function (items) {
+        console.log('Displaying items');
+        var rows = '';
+
+        items.forEach(item => rows += ToDoList.getItemRow(item));
+
+        console.log(rows);
+
+        $('#to-do-items tbody').html(rows);
+    },
+
+    getItems: function () {
+        $.ajax({
+            url: ToDoList.apiURL,
+            method: "GET"
+        }).done(function (response) {
+            console.log(response);
+            //  reload items table
+
+            ToDoList.displayItems(response)
+
+        });
+    },
+
+    deleteItem: function (id) {
+        $.ajax({
+            url: ToDoList.apiURL + '?id=' + id,
+            method: "DELETE"
+        }).done(function (response) {
+            console.log(response);
+            //  reload items table
+
+            ToDoList.getItems();
+        });
     },
 
     bindEvents: function () {
 
-        $("#to-do-items") . submit(function () {
+        $("#create-item-form").submit(function (event) {
             event.preventDefault();
+
+            console.log("submitting form");
 
             ToDoList.addItem();
 
             return false;
 
         });
+         $('#to-do-items tbody').delegate('.delete', 'click', function () {
+            var id = $(this) .data('id');
 
+            ToDoList.deleteItem(id);
+
+         });
     }
 
 };
-
+ToDoList.getItems();
 ToDoList.bindEvents();
